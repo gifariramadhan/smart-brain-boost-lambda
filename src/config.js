f2f074ecb55c2bf254f0da02c79560f1a0726ac5
gifariramadhan <gifariramadhan.gr@gmail.com>
2022-05-25 03:53:22 +0700

    adding components folder and config.js

---
+++ src/config.js
@@ -0,0 +1,59 @@
+export const particlesOptions ={
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 200,
+        enable: true,
+        opacity: 0.7,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 2 },
+      },
+    },
+    detectRetina: true,
+}
