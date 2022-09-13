fe640c5988781271198737959ab458adb58c5e5c
gifariramadhan <gifariramadhan.gr@gmail.com>
2022-09-14 05:34:18 +0700

    adding new feature

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import './FaceRecognition.css';
+import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecogniion = ({ imageURL, box }) => {
+const FaceRecogniion = ({ imageURL, boxes }) => {
     return (
-        <div className='center ma'>
-            <div className='absolute mt2'>
-                <img id='inputImage' alt='' src={imageURL} width='500px' height='auto'/>
-                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        <div className="center ma">
+            <div className="absolute mt2">
+                <img
+                    id="inputImage"
+                    alt=""
+                    src={imageURL}
+                    width="500px"
+                    height="auto"
+                />
+                {boxes.map((box, i) => {
+                    return (
+                        <div
+                            key={i}
+                            className="bounding-box"
+                            style={{
+                                top: box.topRow,
+                                right: box.rightCol,
+                                bottom: box.bottomRow,
+                                left: box.leftCol,
+                            }}
+                        ></div>
+                    );
+                })}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default FaceRecogniion;
